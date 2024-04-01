13bdbeb2472a786c956ca405756dd571e422531f
Johan Swan <jswannieman@gmail.com>
2024-04-02 12:44:22 +1300

    Update App JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useCallback, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [allowNumbers, setAllowNumbers] = useState(false);
+  const [allowSpecialCharacters, setAllowSpecialCharacters] = useState(false);
+  const [newPassword, setNewPassword] = useState(false);
+  const [passwordLength, setPasswordLength] = useState(9);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef();
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText();
+    passwordRef.current?.select();
+  };
+  const setThePassword = useCallback(() => {
+    const passw = "";
+    let passwString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (allowNumbers) {
+      passwString += "0123456789";
+    }
+    if (allowSpecialCharacters) {
+      passwString += "!@#$%^&*()_";
+    }
+    const passwArray = passwString.split("");
+    for (let i = 0; i < passwArray.length; i++) {
+      const character =
+        passwArray[Math.floor(Math.random() * passwArray.length + 1)];
+      passw += character;
+    }
+  }, [allowNumbers, allowSpecialCharacters, passwordLength]);
+
+  useEffect(() => {
+    setNewPassword();
+  }, [allowNumbers, allowSpecialCharacters, passwordLength, newPassword]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="password-generator">
+      <h2>Password Generator</h2>
+      <div className="top-row">
+        <input
+          type="text"
+          value={password}
+          placeholder="password"
+          readonly
+          ref={passwordRef}
+        />
+        <button onClick={copyPasswordToClipboard}>Copy</button>
+        <button onClick={() => setNewPassword((prevPassword) => !prevPassword)}>
+          Generate New Password
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="bottom-row">
+        <div>
+          <label>Password Length : </label>
+          <input
+            type="range"
+            value={passwordLength}
+            min={9}
+            max={20}
+            onChange={(event) => setPasswordLength(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>Allow Numbers : </label>
+          <input
+            type="checkbox"
+            defaultChecked={allowNumbers}
+            onChange={() => setAllowNumbers((prevAllowNbrs) => !prevAllowNbrs)}
+          />
+        </div>
+        <div>
+          <label>Allow Special Characters</label>
+          <input
+            type="checkbox"
+            defaultChecked={allowSpecialCharacters}
+            onChange={() =>
+              setAllowSpecialCharacters((prevAllowSpecial) => !prevAllowSpecial)
+            }
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
